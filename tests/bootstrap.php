dab972c19c3c7dbc38f033cab23ba8fab16ed9eb
Rolf Vreijdenberger <rolf@vreijdenberger.nl>
2014-09-30 22:11:21 +0200

    trying to kill ob_flush error

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-//add the composer autoloader
-require_once __DIR__ . '/../vendor/autoload.php';
 //we use ob_start for some tests that use php sessions
 ob_start();
+//add the composer autoloader
+require_once __DIR__ . '/../vendor/autoload.php';
