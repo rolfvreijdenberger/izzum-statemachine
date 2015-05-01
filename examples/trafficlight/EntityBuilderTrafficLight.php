7740a4f7f1576a0f79a11f08ca561861b687df6d
Rolf Vreijdenberger <rolf@vreijdenberger.nl>
2015-05-01 23:45:34 +0200

    updated example for 2.0.1

--- examples/trafficlight/EntityBuilderTrafficLight.php
+++ examples/trafficlight/EntityBuilderTrafficLight.php
@@ -1,15 +1,15 @@
 <?php
 namespace izzum\examples\trafficlight;
 use izzum\statemachine\EntityBuilder;
-use \izzum\statemachine\Context;
+use \izzum\statemachine\Identifier;
 /**
  * The builder for our Context object.
  * It returns the TrafficLight domain object and will cache it
  * as long as this builder is called via the same context.
  */
 class EntityBuilderTrafficLight extends EntityBuilder{
-    protected function build(Context $context) {
-        $light = new TrafficLight($context->getEntityId());
+    protected function build(Identifier $identifier) {
+        $light = new TrafficLight($identifier->getEntityId());
         return $light;
     }
 }
